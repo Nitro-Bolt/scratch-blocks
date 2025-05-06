e7efcfa585b567ff4ea1fa04541c78d68c466e1f
Cubester <cubester.spamlegends@gmail.com>
2025-05-05 20:04:08 -0400

    Simple reporter test (hot dog :D)

--- blocks_vertical/default_toolbox.js
+++ blocks_vertical/default_toolbox.js
@@ -761,6 +761,7 @@ Blockly.Blocks.defaultToolbox = '<xml id="toolbox-categories" style="display: no
     '<block type="extension_nested_extendable_inputs_test" id="extension_nested_extendable_inputs_test"></block>' +
     '<block type="extension_recursive_extendable_test" id="extension_recursive_extendable_test"></block>' +
     '<block type="extension_extendable_if_test" id="extension_extendable_if_test"></block>' +
+    '<block type="extension_extendable_reporter_test" id="extension_extendable_reporter_test"></block>' +
     '<sep gap="36"></sep>' +
     '<block type="extension_pen_down" id="extension_pen_down"></block>' +
     '<block type="extension_music_drum" id="extension_music_drum">' +
