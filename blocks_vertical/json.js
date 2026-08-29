87c8eab1f835ff07fcfc829f6a77422b351ca10a
Cubester <78769806+CubesterYT@users.noreply.github.com>
2026-08-28 20:30:34 -0400

    Cool new JSON blocks that do illegal things (#44)
    * jsonmapblock
    
    * Use COOL arrow
    
    * Add JSON filter block
    
    * Add JSON sort block NOW!
    
    * Change
    
    ---------
    
    Co-authored-by: ddededodediamante <lukitadiamante5@gmail.com>

--- blocks_vertical/json.js
+++ blocks_vertical/json.js
@@ -695,3 +695,216 @@ Blockly.Blocks['json_foreach_index'] = {
     });
   }
 };
+
+Blockly.Blocks['json_map'] = {
+  /**
+   * Block to map each item and index in array.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.JSON_MAP,
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "ARRAY",
+          "check": "Array"
+        },
+        {
+          "type": "input_value",
+          "name": "VALUE"
+        },
+        {
+          "type": "input_value",
+          "name": "INDEX"
+        },
+        {
+          "type": "field_image",
+          "src": Blockly.mainWorkspace.options.pathToMedia + "icons/json_right_arrow.svg",
+          "width": 24,
+          "height": 24,
+          "alt": "=>"
+        },
+        {
+          "type": "input_value",
+          "name": "METHOD"
+        },
+      ],
+      "category": Blockly.Categories.json,
+      "extensions": ["colours_json", "output_array"],
+    });
+  }
+};
+
+Blockly.Blocks['json_map_value'] = {
+  /**
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.JSON_FOREACH_VALUE,
+      "output": null,
+      "outputShape": Blockly.OUTPUT_SHAPE_ROUND,
+      "category": Blockly.Categories.json,
+      "duplicateOnDrag": true,
+      "extensions": ["colours_json"],
+    });
+  }
+};
+
+Blockly.Blocks['json_map_index'] = {
+  /**
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.JSON_FOREACH_INDEX,
+      "category": Blockly.Categories.json,
+      "duplicateOnDrag": true,
+      "extensions": ["colours_json", "output_number"]
+    });
+  }
+};
+
+Blockly.Blocks['json_filter'] = {
+  /**
+   * Block to filter each item and index in array.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.JSON_FILTER,
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "ARRAY",
+          "check": "Array"
+        },
+        {
+          "type": "input_value",
+          "name": "VALUE"
+        },
+        {
+          "type": "input_value",
+          "name": "INDEX"
+        },
+        {
+          "type": "field_image",
+          "src": Blockly.mainWorkspace.options.pathToMedia + "icons/json_right_arrow.svg",
+          "width": 24,
+          "height": 24,
+          "alt": "=>"
+        },
+        {
+          "type": "input_value",
+          "name": "METHOD",
+          "check": "Boolean"
+        },
+      ],
+      "category": Blockly.Categories.json,
+      "extensions": ["colours_json", "output_array"],
+    });
+  }
+};
+
+Blockly.Blocks['json_filter_value'] = {
+  /**
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.JSON_FOREACH_VALUE,
+      "output": null,
+      "outputShape": Blockly.OUTPUT_SHAPE_ROUND,
+      "category": Blockly.Categories.json,
+      "duplicateOnDrag": true,
+      "extensions": ["colours_json"],
+    });
+  }
+};
+
+Blockly.Blocks['json_filter_index'] = {
+  /**
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.JSON_FOREACH_INDEX,
+      "category": Blockly.Categories.json,
+      "duplicateOnDrag": true,
+      "extensions": ["colours_json", "output_number"]
+    });
+  }
+};
+
+Blockly.Blocks['json_sort'] = {
+  /**
+   * Block to sort each item and index in array.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.JSON_SORT,
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "ARRAY",
+          "check": "Array"
+        },
+        {
+          "type": "input_value",
+          "name": "A"
+        },
+        {
+          "type": "input_value",
+          "name": "B"
+        },
+        {
+          "type": "field_image",
+          "src": Blockly.mainWorkspace.options.pathToMedia + "icons/json_right_arrow.svg",
+          "width": 24,
+          "height": 24,
+          "alt": "=>"
+        },
+        {
+          "type": "input_value",
+          "name": "METHOD"
+        },
+      ],
+      "category": Blockly.Categories.json,
+      "extensions": ["colours_json", "output_array"],
+    });
+  }
+};
+
+Blockly.Blocks['json_sort_a'] = {
+  /**
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.JSON_SORT_A,
+      "output": null,
+      "outputShape": Blockly.OUTPUT_SHAPE_ROUND,
+      "category": Blockly.Categories.json,
+      "duplicateOnDrag": true,
+      "extensions": ["colours_json"],
+    });
+  }
+};
+
+Blockly.Blocks['json_sort_b'] = {
+  /**
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.JSON_SORT_B,
+      "output": null,
+      "outputShape": Blockly.OUTPUT_SHAPE_ROUND,
+      "category": Blockly.Categories.json,
+      "duplicateOnDrag": true,
+      "extensions": ["colours_json"]
+    });
+  }
+};
