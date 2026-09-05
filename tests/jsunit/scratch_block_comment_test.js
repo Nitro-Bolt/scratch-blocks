7f89c3a6edbc22472917b06a1b69e7cbb455119a
Cubester <78769806+CubesterYT@users.noreply.github.com>
2026-09-05 19:43:10 -0400

    Discard changes to tests/jsunit/scratch_block_comment_test.js

--- tests/jsunit/scratch_block_comment_test.js
+++ tests/jsunit/scratch_block_comment_test.js
@@ -23,7 +23,7 @@ goog.require('goog.testing');
 
 var block;
 var workspace;
-var originalCreateIcon = Blockly.ScratchBlockComment.prototype.createIcon;
+var originalCreateIcon = Blockly.ScratchBlockComment.createIcon;
 
 function scratchBlockCommentTest_setUp() {
   workspace = new Blockly.Workspace();
